433d8ded05576c0aae3b2a1ab273608343a36f10
Sera <win10790@gmail.com>
2021-11-11 16:05:50 -0500

    Change home page UI

--- front/components/room-component.js
+++ front/components/room-component.js
@@ -1,41 +1,55 @@
-import Link from 'next/link'
-import Moment from 'react-moment';
-import Graph from './graph';
-import { shortenAddress } from '../utils/shorten-address';
+import Link from "next/link";
+import Moment from "react-moment";
+import { shortenAddress } from "../utils/shorten-address";
 
-const RoomComponent = ({rent}) => {
-    if (!rent) {
-        return
-    }
-    return (
-        <div className="flex">
-            <div className="bg-gray-purple p-4 mb-4 rounded mr-6">
-                <Graph />
-            </div>
-            <div>
-                <div>{rent.location}</div>
-                <div>Rent Date:
-                <Moment format="YYYY-MM-DD">{rent.startDate.toString()}</Moment>
-                &nbsp;~&nbsp;
-                <Moment format="YYYY-MM-DD">{rent.endDate.toString()}</Moment>
-                </div>
-                <div>
-                    <Link href={`/user/${rent.owner}`}>
-                        <a>Owner Address: {shortenAddress(rent.owner)}</a>
-                    </Link>
-                </div>
-                <div>
-                    <Link href={`/user/${rent.tenant}`}>
-                        <a>Tenant Address: {shortenAddress(rent.tenant)}</a>
-                    </Link>
-                </div>
-                <div>{rent.price} eth/month</div>
-                <Link href={`/room/${rent.contractId}`}>
-                    <a>Room Detail</a>
-                </Link>
-            </div>
+const RoomComponent = ({ rent, children, showTenent = true }) => {
+  if (!rent) {
+    return;
+  }
+
+  const isTenantExist = (tenant) => {
+    return tenant && tenant !== "0x0000000000000000000000000000000000000000";
+  };
+
+  return (
+    <div className="bg-gray-purple p-5 rounded flex flex-col border-solid border-4 border-opacity-50">
+      <div className="text-left mb-3">
+        <span className="font-extrabold text-5xl">{rent.price}</span> ETH / month
+      </div>
+      <div>
+        <div>
+          <span className="font-bold">Address: </span>
+          {rent.location}
         </div>
-    )
-}
+        <div>
+          <span className="font-bold">Start date: </span>
+          <Moment format="YYYY-MM-DD">{rent.startDate.toString()}</Moment>
+        </div>
+        <div>
+          <span className="font-bold">End date: </span>
+          <Moment format="YYYY-MM-DD">{rent.endDate.toString()}</Moment>
+        </div>
+        <div>
+          <span className="font-bold">Owner: </span>
+          <Link href={`/user/${rent.owner}`}>
+            <a className="underline">{shortenAddress(rent.owner)}</a>
+          </Link>
+        </div>
+        {showTenent && isTenantExist(rent.tenant) && (
+          <div>
+            <span>Tenant: </span>
+            <Link href={`/user/${rent.tenant}`}>
+              <a className="underline">{shortenAddress(rent.tenant)}</a>
+            </Link>
+          </div>
+        )}
+        <Link href={`/room/${rent.contractId}`}>
+          <a className="underline mt-3">Detail</a>
+        </Link>
+      </div>
+      {children}
+    </div>
+  );
+};
 
-export default RoomComponent
+export default RoomComponent;
